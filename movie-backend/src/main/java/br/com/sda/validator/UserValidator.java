814a16f4b0424d934329b6997b78968b32aeaf7d
Lucas Araújo <lucas.compufc@gmail.com>
2017-04-28 00:27:41 -0300

    Criando classe para validação dos campos do usuário

---
+++ movie-backend/src/main/java/br/com/sda/validator/UserValidator.java
@@ -0,0 +1,46 @@
+package br.com.sda.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import br.com.sda.model.User;
+import br.com.sda.service.impl.UserServiceImpl;
+
+/**
+ * Classe de validação dos campos da classe User.
+ * 
+ * @author Lucas
+ *
+ */
+@Component
+public class UserValidator implements Validator{
+
+	@Autowired
+    private UserServiceImpl userService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+        if (user.getUsername().length() < 4 || user.getUsername().length() > 32) {
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+        if (userService.findByUsername(user.getUsername()) != null) {
+            errors.rejectValue("username", "Duplicate.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 4 || user.getPassword().length() > 16) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+    }
+}
